78691bd1179bbc5139788fee7c9318f349236574
Devesh <deves125@gmail.com>
2023-05-02 16:05:21 +0530

    feat: add content and styling in website

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import MomentumScroll from "@/components/MomentumScroll";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MomentumScroll>
+      <Component {...pageProps} />
+    </MomentumScroll>
+  );
 }
