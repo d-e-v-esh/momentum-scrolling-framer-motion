9b692d3c5f5780492f6d456f612bbb17861e9291
Devesh <deves125@gmail.com>
2023-05-02 16:04:38 +0530

    feat: add TextBlock component

---
+++ src/components/TextBlock.tsx
@@ -0,0 +1,111 @@
+export const TextBlock = () => {
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+        reprehenderit dolor molestias natus facilis animi veritatis minima. Quam
+        veniam exercitationem possimus incidunt tenetur id ab necessitatibus
+        suscipit voluptate modi? Quibusdam, odio asperiores vitae atque quidem
+        eaque dolor quam repellendus eveniet provident nulla ad optio rerum,
+        debitis non officiis porro, totam doloribus tenetur obcaecati ex.
+        Distinctio dolorum praesentium architecto fugiat accusantium nesciunt,
+        illo, hic maiores recusandae dolor nam quia blanditiis corporis sint,
+        doloribus reiciendis totam officia quaerat inventore consectetur tempore
+        necessitatibus minus perspiciatis. Cupiditate nostrum deleniti ipsum
+        explicabo delectus sapiente numquam accusantium, omnis beatae in sint,
+        esse repellat sequi voluptatibus repudiandae?
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur
+        voluptatum mollitia quo? Tempora reiciendis perferendis distinctio
+        dolorum ducimus impedit necessitatibus ut iure velit quo deleniti earum
+        libero, aperiam praesentium corporis laborum illum ipsam vero aut
+        deserunt rem quis. Harum, a adipisci. Velit saepe distinctio, in
+        suscipit dolores vero minus nam porro quos modi eos officiis unde
+        repudiandae esse pariatur non dicta quo iure blanditiis ut consequuntur
+        inventore eveniet perspiciatis! Iure cupiditate unde ea. Fugiat ullam
+        minus tenetur sed voluptate error quia nulla ipsam accusamus illum
+        numquam voluptatibus corrupti, quam magni quidem autem repudiandae
+        distinctio, assumenda quasi deleniti omnis. Ipsam, ullam!
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi illum
+        sequi quisquam earum rem. At magni explicabo voluptate commodi corporis.
+        Enim odio architecto itaque cum. Sint dolorem debitis minima libero eum,
+        assumenda ea. Aut, harum! Dolorem accusantium sint, dicta recusandae
+        sed, vitae, id reiciendis a animi cupiditate impedit? Autem, neque
+        provident earum laboriosam quibusdam ratione eos et dicta quos, laborum,
+        iure soluta quasi recusandae nemo rerum laudantium ullam culpa commodi.
+        Doloremque, recusandae qui asperiores nam deleniti odio praesentium
+        deserunt consequatur architecto, consequuntur ratione vitae porro vel,
+        voluptatem quo facilis nulla harum voluptatum at fugiat tempora cumque
+        animi ea velit? Obcaecati facilis ratione voluptatum distinctio tempore
+        doloremque quisquam perspiciatis. Ipsam quia praesentium minima
+        repudiandae consequuntur hic, excepturi soluta suscipit ratione eius,
+        incidunt consequatur sunt? Unde eum, iste aliquid ea neque tempore
+        veritatis voluptatum molestiae velit at animi vel explicabo commodi ab
+        consectetur illo. Ipsa velit eaque, id veritatis, corrupti ex aspernatur
+        molestiae nisi, consequuntur beatae quod exercitationem voluptas maxime
+        at. Adipisci rem aliquid obcaecati facere deserunt voluptatem cum. Unde,
+        atque ab! Dolores nesciunt corporis a architecto. Asperiores fuga
+        molestiae magni adipisci similique eligendi est ullam odio placeat.
+        Totam quas laborum quibusdam quos necessitatibus numquam provident, qui
+        est molestias nisi, voluptatem aspernatur.
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, dolor
+        consectetur, autem ipsum, officia aut quia itaque placeat dolore omnis
+        non deleniti dicta facilis aspernatur fugiat harum? Perferendis quo
+        atque, minima dolores illo temporibus doloribus accusantium iure fugit
+        officiis amet. Magnam sunt incidunt vero perferendis fugit sapiente
+        tenetur minus unde?
+      </p>
+
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor vero non
+        inventore! Unde quibusdam vel autem obcaecati deleniti voluptas dolorum?
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo labore,
+        placeat officia beatae dolor ipsum sed unde assumenda rem! Optio sequi
+        praesentium autem. Possimus error, accusamus nihil facere ullam ad
+        voluptatibus suscipit dignissimos repellat, quos voluptas deleniti,
+        magnam nisi. Doloribus commodi facere, hic tempora ducimus ab soluta
+        minus expedita quam ad distinctio, esse omnis animi aperiam? Quos neque
+        sunt nisi, illum voluptatem nam deleniti labore nihil temporibus ullam
+        distinctio aliquid? Ab ea reprehenderit quia sunt veritatis recusandae
+        laboriosam aspernatur architecto? Laborum autem assumenda earum quis
+        officiis quod. Consectetur dolore aspernatur sunt tenetur quod optio
+        esse ducimus possimus, officia laborum repudiandae?
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
+        repellendus quod vero, suscipit laborum magni tempora odio et dolores!
+        Incidunt, sequi. Id accusantium autem incidunt eos, quia perferendis
+        consequuntur, nam, veniam cupiditate ex mollitia quos harum repudiandae.
+        Ipsam eius illo voluptatum ea ratione autem neque, quia voluptates cum
+        officia provident cumque optio velit aliquid laboriosam error facere
+        dolore recusandae, minima quisquam mollitia iure dolorem? Ea dicta quae
+        non doloribus. Nam ab laudantium optio repellendus temporibus, odio modi
+        veniam, cum rerum et ducimus minima laborum quam a atque neque alias
+        voluptates pariatur soluta vel ipsum quaerat est velit. Dicta magni
+        consequuntur provident exercitationem officiis minus dolorum mollitia?
+        Asperiores alias minus saepe obcaecati a corrupti assumenda aspernatur
+        reiciendis aliquid expedita consequatur, soluta dignissimos nobis magnam
+        eum quam mollitia maiores vero facilis quas unde hic possimus quos!
+        Tenetur recusandae cum fuga iure fugit eius corporis perferendis quasi
+        unde totam eveniet, magnam laboriosam odio reiciendis ex, voluptas aut
+        non beatae rerum nulla. Odit, quisquam esse quia iure sunt consequuntur
+        rem, placeat adipisci dolores, tenetur voluptatibus et voluptate dolorem
+        hic quibusdam earum ab! Aperiam vel iste ea tempora amet quam quos
+        quaerat harum dolores omnis sed exercitationem consectetur quidem minus
+        aspernatur, inventore nemo delectus neque.
+      </p>
+    </div>
+  );
+};
